aaf6656883267bc0243dd3be201417b2509a9d52
MusdholiHolifah <alveerhaolive@gmail.com>
2022-11-28 22:48:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kainapp/Sutra.java
@@ -0,0 +1,15 @@
+package com.example.kainapp;
+import com.google.gson.annotations.SerializedName;
+
+public class Sutra {
+    private int NIM;
+    private String Nama;
+
+    public int getNIM() {
+        return NIM;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+}
